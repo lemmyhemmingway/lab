bdb998017c06d1858d86175ccd5347295a86a455
yasincaykur <yasincaykur@gmail.com>
2025-09-13 18:21:21 +0300

    add maps

---
+++ golang/go-with-tests/maps/dictionary.go
@@ -0,0 +1,17 @@
+package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definiton, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definiton, nil
+}
+
